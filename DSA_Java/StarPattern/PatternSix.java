a40259237101ac6e30355401494244c5c3b26c59
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-07-21 15:16:26 +0530

    List Updated

---
+++ DSA_Java/StarPattern/PatternSix.java
@@ -0,0 +1,19 @@
+package DSA_Java.StarPattern;
+
+/*  1 2 3 4 5
+    1 2 3 4
+    1 2 3
+    1 2
+    1
+ */
+public class PatternSix {
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <=5; i++) {
+            for (int j = 5 , p = 1; j>=i; j-- , p ++) {
+                System.out.print( p + " ");
+            }
+            System.out.println();
+        }
+    } 
+}
