cd82b4538dbea463951423e40f2ec01d2c2a9d8f
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-07-21 14:12:11 +0530

    List Updated

--- DSA_JS/Math/PowerOfTwo.js
+++ DSA_JS/Math/PowerOfTwo.js
@@ -1,17 +1,15 @@
 // @Problem : Determine if the number is power of 2 or not
-// @Input : 8  22  
+// @Input : 8  22
 // @Output : true false
-
 
 PowerOfTwo = (n) => {
     if (n < 1) return false;
     while (n > 1) {
         if (n % 2 == 0) {
-            n /= 2
-        }
-        else return false;
+            n /= 2;
+        } else return false;
     }
     return true;
-}
+};
 
-console.log(PowerOfTwo(33))
+console.log(PowerOfTwo(33));
