c91bd9afdf477ab8de4c9e5de1d9f5727fe88aaf
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-07-23 00:08:30 +0530

    List Updated

---
+++ DSA_Java/StarPattern/Pattern8.java
@@ -0,0 +1,30 @@
+package DSA_Java.StarPattern;
+
+
+
+/* 
+
+*********
+ *******
+  *****
+   ***
+    *
+
+*/
+public class Pattern8 {
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 5; i++) {
+           for (int j = 0; j < i; j++) {
+             System.out.print(" ");
+           }
+           for (int j = 5; j>i ; j--) {
+                System.out.print("*");
+           }
+           for (int j = 4; j > i ; j--) {
+            System.out.print("*");
+           }
+           System.out.println();
+        }
+    }
+}
