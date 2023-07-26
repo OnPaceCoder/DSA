22a5eb34131d37ad109b2b55c02d81c723614987
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-07-26 12:52:35 +0530

    List Updated

---
+++ DSA_Java/Recursion/SumN.java
@@ -0,0 +1,13 @@
+package DSA_Java.Recursion;
+
+public class SumN {
+
+    static int sum(int n){
+        if(n == 0 ) return 0;
+        return n + sum(n-1);
+    }
+        public static void main(String[] args) {
+            
+            System.out.println(sum(5));
+        }
+}
