33c92813ee356a4fdfda49440a8f376ac672708e
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-07-26 12:39:33 +0530

    List Updated

--- DSA_Java/Recursion/PrintN.java
+++ DSA_Java/Recursion/PrintN.java
@@ -1,19 +1,28 @@
 package DSA_Java.Recursion;
 
 public class PrintN {
-
+//Method 1:
     static void Print(int i , int n){
         if(i > n) return;
         System.out.println(i);
         Print(i+1,n);
     }
 
+
+    //Method 2:
+    static void Print2(int i ){
+        if(i < 1) return;
+        Print2(i-1);
+        System.out.println(i);
+    }
     public static void main(String[] args) {
         
 
         int n =5;
+        System.out.println("Method 1 :");
         Print(1, n);
 
-        
+        System.out.println("Method 2 :");
+        Print2(n );
     }    
 }
--- DSA_Java/Recursion/PrintNRev.java
+++ DSA_Java/Recursion/PrintNRev.java
@@ -3,15 +3,28 @@ package DSA_Java.Recursion;
    
 
 public class PrintNRev {
-
+//Method:1 
     static void Print(int n){
         if(n<1) return ;
         System.out.println(n);
         Print(n-1);
     }
+
+
+    //Method:2 
+    static void Print2(int i , int n){
+        if(1 > i) return;
+        Print2(i-1 , n+1);
+        System.out.println(n);
+        
+    }
         public static void main(String[] args) {
-            
+                System.out.println("Method 1:");
             int n = 5;
             Print(n);
+
+            System.out.println("Method 2:");
+
+            Print2(n , 1);
         }
 }
