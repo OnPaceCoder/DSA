2071a8b28d1a4a6555a1ea11c75e6d70a2e56bb4
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-07-28 23:57:45 +0530

    List Updated

---
+++ DSA_Java/Recursion/Fibonacci.java
@@ -0,0 +1,16 @@
+package DSA_Java.Recursion;
+
+public class Fibonacci {
+    
+    public static int fibo(int n) {
+        if(n<2) return 1 ;
+    
+
+         return fibo(n-1) + fibo(n-2);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(fibo(3));
+    }
+}
