d4479befd5c7db61acac7af18ac40d44bae0b2ff
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-07-30 17:13:23 +0530

    List Updated

---
+++ DSA_Java/Hashing/HashingNumber.java
@@ -0,0 +1,50 @@
+package DSA_Java.Hashing;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class HashingNumber {
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+            
+        //Input Size of Array
+        int size = in.nextInt(); //5
+              
+            int[] arr = new int[size];   //arr = [1 3 1 2 4]
+        //Input Elements of Array
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = in.nextInt();
+            }
+            
+
+            //PreCompute
+            // int[] hash = new int[10];
+            HashMap<Integer,Integer> hash=new HashMap<Integer,Integer>();
+
+            for (int i = 0; i < arr.length; i++) {
+                    // hash[arr[i]] +=1;
+                    hash.put(arr[i] , hash.getOrDefault(arr[i] , 0) + 1);
+            }
+
+            //Input Number of Test Cases
+            int q = in.nextInt();
+            while(q>0) {
+                //Input Elements to be checked for test case
+                int number = in.nextInt();
+                // System.out.println(hash[number]);
+                System.out.println(hash.getOrDefault(number , 0));
+                
+                //Fetch
+                q--;
+            }
+            
+            in.close(); 
+            
+   
+       
+
+
+
+    }
+}
